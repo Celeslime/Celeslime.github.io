2fc48308cfed643cc183880bbb0af3d54028a155
矰 <41530571+Celeslime@users.noreply.github.com>
2022-07-18 18:47:24 +0800

    new

---
+++ class-tri/山东师大附中 2018 级 3 班毕业蹭饭地图_files/main.js
@@ -0,0 +1,224 @@
+﻿// 这个地图参考了 https://github.com/Heriyadi235/17dbsyg2cft，在此致谢
+
+var dom = document.getElementById("main");
+var myChart = echarts.init(dom, 'shine');
+option = null;
+
+var mainColor = '#f3f8ff';
+var textColor = '#9da5b3';
+var borderColor = '#029fd4';
+var spotColor = '#ff9070';
+var tooltipColor = '#c8c841';
+
+var os = function () {
+    var ua = navigator.userAgent,
+        isWindowsPhone = /(?:Windows Phone)/.test(ua),
+        isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+        isAndroid = /(?:Android)/.test(ua),
+        isFireFox = /(?:Firefox)/.test(ua),
+        isChrome = /(?:Chrome|CriOS)/.test(ua),
+        isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+        isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+        isPc = !isPhone && !isAndroid && !isSymbian;
+    return {
+        isTablet: isTablet,
+        isPhone: isPhone,
+        isAndroid: isAndroid,
+        isPc: isPc
+    };
+}();
+
+option = {
+    title: {
+        text: '山东师大附中 2018 级 3 班毕业蹭饭地图（交互版）',
+        // subtext: '班主任：傅平修\n\n任课老师：孙国旺、傅平修、颜廷波、曹立、宋志刚、王凤芝、李伟、王醒、牛丽华、孙晓峰',
+        left: 'center',
+        textStyle: { color: textColor, fontSize: 30 },
+        subtextStyle: { color: textColor, fontSize: 15 }
+        // padding: 100
+    },
+    tooltip: {
+        trigger: 'item',
+        //triggerOn:'mousemove',
+        hideDelay: 333,
+        confine: true, // 是否约束 content 在 viewRect 中。默认 false 是为了兼容以前版本。
+        textStyle: {
+            fontWeight: 'regular',
+            fontFamily: 'Yahei',
+            fontSize: 22,
+            backgroundColor: 'rgba(40, 44, 52, 0.5)',
+            color: tooltipColor
+        },
+        formatter: function (params) { return params.name + '（' + params.value[3].split(' ')[1] + '）<br />' + params.value[2]; }
+    },
+    toolbox: {
+        show: true,
+        orient: 'vertical',
+        itemSize: 32,
+        left: 'right',
+        top: 'center',
+        feature: {
+            // saveAsImage: {},
+            // 这个自带的存图功能不能保存背景图，太丑，不要他了
+            // 但是图标挺好看的，可以直接修改对应的第 79191 行 echarts.js 代码
+            myAbout:
+            {
+                show: true,
+                title: '关于',
+                    icon: 'path://M25.5,42.7h-4.4c-1.3,0-3.3-0.5-5.3-1.5c-2-1-4.1-2.4-5.7-4.2l-4.4-4.8l-4.4-4.8c-0.4-0.4-0.8-1-0.9-1.7s0.1-1.6, 0.9-2.4c0.9-0.8, 1.8-1.1, 2.6-1.2s1.5, 0.1, 1.8, 0.2l2.9, 2.2l2.9, 2.2L11.7, 15l0.2-11.7c0- 0.4, 0.1-1.2, 0.5-1.8c0.5-0.6, 1.3-1.1, 2.9-1.1s2.5, 0.5, 3, 1.1C18.9, 2.1, 19, 2.8, 19, 3.3v8v8c0 - 1.1, 0.1-2.5, 0.5-3.6c0.4-1.2, 1.3-2, 2.8-2c1.5, 0, 2.5, 0.7, 3.1, 1.5c0.6, 0.8, 0.8, 1.8, 0.8, 2.5c0.1-0.3, 0.4-0.9, 1-1.4c0.6-0.5, 1.5-0.8, 2.7-0.8c1.6, 0, 2.5, 0.6, 3, 1.2c0.5, 0.6, 0.6, 1.4, 0.6, 1.7v0.5v0.5c0.2-0.3, 0.4-0.8, 0.9-1.2s1.3-0.7, 2.7-0.7c1.5, 0, 2.3, 0.4, 2.8, 0.9s0.6, 1.2, 0.6, 1.6v6.2v6.2c0, 1.8-1.1, 4.4-2.9, 6.6S33, 42.7, 30, 42.7l - 3.9 - 0.1',
+                onclick: function () {
+                    if (option.title.left == 'center') {
+                        option.geo.roam = true;
+                        option.title.left = 'left';
+                        option.title.text = '山东师大附中 2018 级 3 班毕业去向详情';
+                        // option.title.subtext = '广东\n    南方科技大学：朱骐、杨晓荣、苏茹涵\n    华南理工大学：任希晗\n上海\n    上海科技大学：刘昱\n    上海对外经贸大学：鹿雨桐\n四川\n    四川大学：高淮峰\n    西南石油大学：张贤明\n    成都理工大学：李庆虎\n    中国民用航空飞行学院：董雨佳佳\n山东\n    山东大学：周涵森、张泽、李逸霏、陈奕昂\n    中国石油大学：张钛然\n    山东师范大学：杨若涵、武原禾、郭君婕、周安木\n    青岛大学：赵汝恒、尤胜寒、翟松亭\n    齐鲁工业大学：马翊轩\n    山东中医药大学：李芃泽\n    济南大学：鲁畅、王宜萱\n    山东财经大学：纪翔\n    山东农业大学：孙竹妍\n    烟台大学：胡琮林\n陕西\n    西安电子科技大学：于洋\n    西安建筑科技大学：胡松昀\n浙江\n    杭州电子科技大学：徐沁发\n    宁波诺丁汉大学：孟晓彤\n江苏\n    南京农业大学：朱茂隆\n    中国矿业大学：李木子\n    西交利物浦大学：赵雨晨\n安徽\n    合肥工业大学：刘明昊、张家才\n    安徽理工大学：赵正睿\n    中国科学技术大学：徐明甲\n福建\n    福州大学：杨云飞\n    集美大学：吴宇龙\n    福建农林大学：支俏\n辽宁 东北大学：仝家驹\n海南 海南大学：徐了然\n湖南 国防科技大学：杨一凡\n天津 天津工业大学：倪想\n澳门 澳门科技大学：臧铭辰\n新疆 新疆医科大学：黄轲欣\n';
+                    }
+                    else {
+                        option.title.left = 'center';
+                        option.title.text = '山东师大附中 2018 级 3 班毕业蹭饭地图（交互版）';
+                        // option.title.subtext = '班主任：傅平修\n\n任课老师：孙国旺、傅平修、颜廷波、曹立、宋志刚、王凤芝、李伟、王醒、牛丽华、孙晓峰';
+                    }
+                    myChart.setOption(option, true);
+                }
+            }
+        },
+    },
+    geo: {
+        // zoom: 1.3,
+        map: 'china',
+        label: {
+            show: false
+        },
+        roam: true,
+        // roam: os.isPc, // 如果 roam 开启 true，在手机上缩放会稍微有一点卡顿
+        itemStyle: {
+            normal: {
+                areaColor: mainColor,
+                borderColor: borderColor
+            },
+            emphasis: {
+                areaColor: mainColor
+            }
+        },
+        silent: false // 若 silent 设为 true，则鼠标移至相应地区则不会显示省份名，点也不会有反应
+
+    },
+    series: [{
+        name: '大学',
+        type: 'scatter',
+        coordinateSystem: 'geo',
+        data: convertData(),
+        symbol: os.isPc ? 'pin' : 'circle',
+        // symbol: 'pin',
+        symbolSize: 30,
+        label: {
+            normal: {
+                formatter: '{b}',
+                position: 'right',
+                show: false
+            }
+        },
+        itemStyle: {
+            normal: {
+                color: spotColor
+            }
+        }
+
+    },
+
+    ]
+};
+
+
+var dontBack = false; // 用于实现双击返回上一级地图
+var parentMaps = new Array(); // 维护一个 array，用于记录地图路径
+
+function checkUrl() {
+    var url = window.location.href;
+    if (url.indexOf('#') == -1) return 'china';
+    place = url.substr(url.indexOf('#') + 1);
+    place = decodeURI(place);
+    if (MAPS.indexOf(place) != -1) {
+        // dontBack = true;
+        parentMaps.push('china');
+        option.geo.label.show = true;
+        return place;
+    }
+    else return 'china';
+};
+
+option.geo.map = checkUrl();
+
+
+if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+    myChart.resize();
+    setTimeout(function () { alert("使用说明：\n1) 点一下大学对应的橙色标记，会显示出在这个大学的同学名单\n2) 点一下每个省份的地图，将会进入到各个省份的高清大图模式\n3) 对于人数较多的区，也提供高清大图，可以点\n4) 点两下背景图片将返回到上一级地图\n5) 如果使用电脑查看此蹭饭图，还可以直接用鼠标滚轮进行缩放\n6) 地图右侧小手按钮重置地图\n7) 网页改编自 https://ssfz.top 山东师大附中 2018 级 10 班毕业蹭饭地图") }, 500);
+}
+
+function changeMap(newPlace) {
+    function changeSubtitle() {
+        var stuList = new Array();
+        for (var i = 0; i < DATA.length; ++i) {
+            if (DATA[i].city.indexOf(newPlace) != -1) {
+                var tmp = DATA[i].value.split(' ');
+                for (var j = 0; j < tmp.length; ++j) stuList.push(tmp[j]);
+            }
+        }
+        var str = '';
+        for (var i = 0; i < stuList.length; ++i) {
+            str += i == stuList.length - 1 ? stuList[i] : stuList[i] + ' ';
+            if ((i + 1) % 5 == 0) str += '\n\n';
+        }
+        // option.title.subtext = str;
+    }
+
+    option.title.left = 'center';
+    option.geo.label.show = (newPlace != 'china');
+    if (newPlace != 'china') {
+        option.title.text = '山东师大附中 2018 级 3 班毕业蹭饭地图 - ' + newPlace;
+        changeSubtitle(newPlace);
+    } else {
+        option.title.text = '山东师大附中 2018 级 3 班毕业蹭饭地图（交互版）';
+        // option.title.subtext = '班主任：傅平修\n\n任课老师：孙国旺、傅平修、颜廷波、曹立、宋志刚、王凤芝、李伟、王醒、牛丽华、孙晓峰';
+    }
+    option.geo.map = newPlace;
+    myChart.setOption(option, true);
+}
+
+myChart.on('click', function (params) {
+    if (params.name == '南海诸岛') {
+    }
+    // dontBack = true;
+    // 显示对应省份的大图，并忽视对于校名大头钉的 click 事件
+    // 进入各省地图之后，可以显示城市的名字。
+    // 出来之后再关掉。
+    if (MAPS.indexOf(params.name) != -1) {
+        parentMaps.push(option.geo.map);
+        changeMap(params.name);
+    }
+});
+
+// 由于拖拽地图也会触发 click 事件，所以这里判断一下鼠标按下和抬起的位置坐标变化
+var downX, downY;
+$('#main').mousedown(function (e) {
+    downX = e.pageX;
+    downY = e.pageY;
+});
+
+$('#main').mouseup(function (e) {
+    var upX = e.pageX;
+    var upY = e.pageY;
+    var dltX = Math.abs(upX - downX);
+    var dltY = Math.abs(upY - downY);
+    // console.log(dltX, dltY);
+    if (dltX <= 5 && dltY <= 5 && parentMaps.length > 0) {
+        if (!dontBack) {
+            changeMap(parentMaps[parentMaps.length - 1]);
+            parentMaps.pop();
+            // dontBack = true; // 保证双击返回上一层地图的重要操作
+        } else {
+            // dontBack = false;
+        }
+    }
+});
--- syw/js/main.js
+++ syw/js/main.js
@@ -1,30 +1,34 @@
 // 奇妙的事情发生了 o‿≖✧
-var atkBase,atkAdd,critRate,critDmg;
+var atkBase,atkAdd,critRate,critDmg,elemMastery;
 var exp=new Array(),expEM=new Array(),healthy=new Array(),expBase;
-var maxA=0,maxB=0;
-var A=0.0495,
-	B=0.033,
-	C=0.066,
-	D=16.5,//小公鸡
-	E=19.835 ;//精通
+var maxA=0,maxB=0,RATIO=2.00;
+
+//以下数据由副词套保留两位小数后取平均值获得
+var A=0.0496, //攻击力百分比
+	B=0.033,  //啧
+	C=0.066,  //啧啧
+	D=16.535, //攻击力
+	E=19.815 ;//精通
+//结果保留位数
 var FixNum=1;
-var RATIO=1.25;
+//搞颜色(*/ω＼*)
 var yellow='#ffa',
  	red='#fba',
  	orange='#fec',
  	green='#afd';
+//交互
 function onKeyUp(event){
-	atkBase=getValue('atkBase');
-	atkAdd=getValue('atkAdd');
-
-	critRate=getValue('critRate')/100;
-	critDmg=getValue('critDmg')/100;
-
-	elemMastery=getValue('elemMastery');
-	
+	getValues();
 	check();
 	calc();
 	print();
+}
+function getValues(){
+	atkBase=getValue('atkBase');
+	atkAdd=getValue('atkAdd');
+	critRate=getValue('critRate')/100;
+	critDmg=getValue('critDmg')/100;
+	elemMastery=getValue('elemMastery');
 }
 function getValue(name){
 	var elem=document.forms[0][name]; 
@@ -36,96 +40,57 @@ function getValue(name){
 	}
 	return n;
 }
-class ATK{
-	constructor(base=0,add=0){
-		this.Base=base;
-		this.Add=add;
-	}
-	getATK(rate){
-		return Base*(1+rate)+Add;
-	}
-}
-class CRIT{
-	constructor(rate=0,dmg=0){
-		this.Rate=rate;
-		this.DMG=dmg;
-	}
-}
-class Vector{
-	constructor(atk=ATK(),crit=CRIT(),elemMastery=0){
-		this.ATK=atk;
-		this.CRIT=critRate;
-		this.ElemMastery=elemMastery;
-	}
-	getExp(){
-		return ATK.getATK();
-	}
-}
-
+//算法
 function calc(){
-	expBase         =getExp(atkBase+atkAdd,critRate,critDmg,elemMastery);
-	expBaChong1     =getExpBaChong1(atkBase+atkAdd,critRate,critDmg,elemMastery)
-	expOverloaded   =getExpOverloaded(atkBase+atkAdd,critRate,critDmg,elemMastery);
-	expBaChong      =getExpBaChong(atkBase+atkAdd,critRate,critDmg,elemMastery);
-
-	exp['atk']      =getExp(atkBase*(1+A)+atkAdd,critRate,critDmg,elemMastery)-expBase;
-	exp['atkSmall'] =getExp(atkBase+atkAdd+D,critRate,critDmg,elemMastery)-expBase;
+	expBase=getExp(atkBase+atkAdd,critRate,critDmg,elemMastery);
+	expOverloaded=getExpOverloaded(atkBase+atkAdd,critRate,critDmg,elemMastery);
+	//虽说是偏导数，但...的效果会更好
+	exp['atk']=getExp(atkBase*(1+A)+atkAdd,critRate,critDmg,elemMastery)-expBase;
+	exp['atkSmall']=getExp(atkBase+atkAdd+D,critRate,critDmg,elemMastery)-expBase;
 	exp['elemMastery']=getExp(atkBase+atkAdd,critRate,critDmg,elemMastery+E)-expBase;
-	exp['critRate'] =getExp(atkBase+atkAdd,(critRate+B)>=1?1:(critRate+B),critDmg,elemMastery)-expBase;
-	exp['critDmg']  =getExp(atkBase+atkAdd,critRate,critDmg+C,elemMastery)-expBase;
-	exp['BaChong']  =getExpBaChong1(atkBase+atkAdd,critRate,critDmg,elemMastery+E)-expBaChong1;
+	exp['critRate']=getExp(atkBase+atkAdd,critRate+B,critDmg,elemMastery)-expBase;
+	exp['critDmg']=getExp(atkBase+atkAdd,critRate,critDmg+C,elemMastery)-expBase;
 	getMaxA();
-
-	
+	//在这里以超载为一个基准量
 	expEM['Overloaded']=getExpOverloaded(atkBase+atkAdd,critRate,critDmg,elemMastery+E)-expOverloaded;
-	expEM['BaChong']   =getExpBaChong(atkBase+atkAdd,critRate,critDmg,elemMastery+E)-expBaChong;
 	getMaxB()
-
+	//这里用拉格朗日约束的一个推导
 	var scouce=(atkAdd/atkBase)/A+critRate/B+critDmg/C,s1=scouce+1/3;
 	var expAllATK=getExp(1+A*(scouce-0.05/B-0.50/C),0.05,0.50,0);
 	var hRate=(s1+Math.sqrt(s1*s1-1.5))/6,hATK=1/8/hRate+hRate-1/3;
 	var expBest=getExp(1+A*hATK,B*hRate,C*hRate,0);
+	//极值点特判
 	if(expBest>expAllATK){
 		healthy['atkAdd']=atkBase*A*hATK;
 		healthy['critRate']=B*hRate;
 		healthy['critDmg']=C*hRate;
+		if(healthy['critRate']>1){
+			scouce-=1/B;//去除暴击率
+			hATK=(3*C*scouce-1)/(4*A+3*C);
+			hRate=scouce-hATK;
+			expBest=getExp(1+A*hATK,1,C*hRate,0);
+			healthy['atkAdd']=atkBase*A*hATK;
+			healthy['critRate']=1;
+			healthy['critDmg']=C*hRate;
+		}
 	}
 	else{
 		healthy['atkAdd']=atkBase*A*(scouce-0.05/B-0.50/C)
 		healthy['critRate']=0.05;
 		healthy['critDmg']=0.50;
 	}
-
-
-	var a=2,b=-(atkBase+atkAdd)*C/(atkBase*A),c=1;
-	healthy['critRate0']=100*(-b+Math.sqrt(b*b-4*a*c))/2/a;
-	if(healthy['critRate0']>100){
-		healthy['critRate0']=100;
-		healthy['critDmg0']=100*(atkBase+atkAdd)*C/A/atkBase-100;
-	}else{
-		healthy['critDmg0']=C/B*healthy['critRate0']
-	}
-
-	healthy['critRate1']=100*(-b-Math.sqrt(b*b-4*a*c))/2/a;
-	if(healthy['critRate1']>100){
-		healthy['critRate1']=100;
-		healthy['critDmg1']=100*(atkBase+atkAdd)*C/A/atkBase-100;
-	}else{
-		healthy['critDmg1']=C/B*healthy['critRate1']
-	}
 }
 function check() {
-	if(critRate<0.05){
-		critRate=0.05;
-		document.forms[0]['critRate'].value=5.0;
-	}
-	if(critRate>1){
-		critRate=1;
-		document.forms[0]['critRate'].value=100.0;
-	}
+	//暴击率《通常》不低于5%
+	//暴击伤害不低于50%
 	if(critDmg<0.5){
 		critDmg=0.5;
 		document.forms[0]['critDmg'].value=50.0;
+	}
+	//元素精通不小于0
+	if(elemMastery<0){
+		elemMastery=0;
+		document.forms[0]['elemMastery'].value=0;
 	}
 }
 function getMaxA(){
@@ -138,7 +103,7 @@ function getMaxA(){
 	}
 }
 function getMaxB(){
-	maxB=expEM['BaChong'];
+	maxB=expEM['Overloaded'];
 	var i;
 	for(i in expEM){
 		if(expEM[i]>maxB && i!='score'){
@@ -155,19 +120,77 @@ function getOverloaded(elemMastery){
 }
 /****伤害****/
 function getExp(atk,cR,cD,eM){
+	if(cR>1)cR=1;
+	if(cR<0)cR=0;
 	return atk*(1+cR*cD)*(1+getEMRate(eM));
 }
 function getExpOverloaded(atk,critRate,critDmg,elemMastery){
 	return atk*(1+critRate*critDmg)+getOverloaded(elemMastery)/RATIO;
 }
-/****八重神子****/
-function getExpBaChong(atk,critRate,critDmg,elemMastery){
-	return atk*(1+critRate*critDmg)*(1+0.0015*elemMastery)+getOverloaded(elemMastery)/1.706;
+
+
+
+
+//输出 自由排版
+function print(){
+	sc.innerHTML=p('攻击力 ATK:')+div(atkBase+atkAdd)+'<br>';
+
+	sc.innerHTML+=h("综合分数 Score:");
+	sc.innerHTML+=div(expBase.toFixed(FixNum))+"<br>";
+
+	var scFix=getExp(atkBase+healthy['atkAdd'],healthy['critRate'],healthy['critDmg'],elemMastery);
+	var fixVal=scFix-expBase,fixColor=green;
+	if(maxA<fixVal){
+		fixColor=red;
+		maxA*=1.2
+	}
+	sc.innerHTML+=h("修正数据 Fixed:");
+	sc.innerHTML+=p('攻击力 ATK:')+div(atkBase+' + '+healthy['atkAdd'].toFixed(0)+' = '+(atkBase+healthy['atkAdd']).toFixed(0));
+	sc.innerHTML+=p('暴击率 暴击伤害 CRIT:')+div((100*healthy['critRate']).toFixed(0)+'%+'+(100*healthy['critDmg']).toFixed(0)+"%");
+	sc.innerHTML+=p('修正分数增量 Fixed:')+div(fixVal.toFixed(FixNum),fixVal/maxA>1?1:fixVal/maxA,fixColor)+'<br>';
+
+	ex.innerHTML=h("偏导数/偏增量(每份副词条) Partial Derivative：");
+	colum("攻击力百分比 ATK.Rate: ",exp['atk'],green,maxA);
+	colum("攻击力 ATK: ",exp['atkSmall'],green,maxA);
+	colum("元素精通.增幅 下位元素1.5 上位元素2.0 Elemental Mastery: ",exp['elemMastery'],green,maxA);
+	colum("暴击率 CRIT.Rate: ",exp['critRate'],green,maxA);
+	colum("暴击伤害 CRIT.DMG: ",exp['critDmg'],green,maxA);
+	ex.innerHTML+="<br>";
+
+	ex.innerHTML+=h("元素精通90级剧变 (单次触发) Elemental Mastery: ");
+	colum("扩散*2.044(风套:60%伤害 40%减抗) 可能触发其他剧变",expEM['Overloaded']*0.3*2.044,orange,maxB);
+	colum("超载 火伤 爆炸削韧",expEM['Overloaded'],orange,maxB);
+	colum("感电 共存 连续反应",expEM['Overloaded']*0.6,orange,maxB);
+	colum("超导 冰伤 40%减抗",expEM['Overloaded']*0.25,orange,maxB);
+	colum("冻结 碎冰",expEM['Overloaded']*0.75,orange,maxB);
 }
-function getExpBaChong1(atk,critRate,critDmg,elemMastery){
-	return atk*(1+critRate*critDmg)*(1+0.0015*elemMastery);
+function colum(text,num,color,max){
+	ex.innerHTML+=p(text)+DIV(num,num/max,color);
 }
-/****操作函数****/
+function bar(text,num){
+	ex.innerHTML+=p(text)+DIV(num);
+}
+function div(text,rate=1,color='#f9f9f9'){
+	return  '<div class="bg">'+
+				'<p class="num" style="width:'+(100*rate)+'%;background:'+color+';">'+
+					text+
+				'</p>'+
+			'</div>';
+}
+function DIV(text,rate=1,color='#f9f9f9'){
+	if(!isNaN(parseFloat(text)))
+		text=text.toFixed(FixNum);
+	return div(text,rate,color);
+}
+function h(text){
+	return '<h1>'+text+'</h1>';
+}
+function p(text){
+	return '<p>'+text+'</p>';
+}
+
+
+//操作函数 保留
 function fatk(i=1){
 	var elem=document.forms[0]['atkAdd'];
 	elem.value=(getValue('atkAdd')+getValue('atkBase')*A*i).toFixed(FixNum);
@@ -188,72 +211,3 @@ function felemMastery(i=1){
 	elem.value=(getValue('elemMastery')+E*i).toFixed(FixNum);
 	onKeyUp();
 }
-/****输出****/
-function print(){
-	sc.innerHTML=p('攻击力 ATK:')+div(atkBase+atkAdd)+'<br>';
-
-	sc.innerHTML+=h("综合分数 Score:");
-	sc.innerHTML+=div(expBase.toFixed(FixNum))+"<br>";
-
-	sc.innerHTML+=h("修正数据 Fixed:");
-	sc.innerHTML+=p('攻击力 ATK:')+div((atkBase+healthy['atkAdd']).toFixed(0)+' = '+atkBase+' + '+healthy['atkAdd'].toFixed(0));
-	sc.innerHTML+=p('暴击率 暴击伤害 CRIT:')+div((100*healthy['critRate']).toFixed(0)+'%+'+(100*healthy['critDmg']).toFixed(0)+"%");
-	var scFix=getExp(atkBase+healthy['atkAdd'],healthy['critRate'],healthy['critDmg'],elemMastery);
-	sc.innerHTML+=p('修正分数 Score.Fixed:')+div(scFix.toFixed(FixNum)+' (+'+(100*(scFix/expBase-1)).toFixed(FixNum)+'%)')+'<br>';
-
-	ex.innerHTML=h("偏导数(副词条参数) Partial Derivative：");
-	colum("攻击力(大攻击) ATK: ",exp['atk'],green,maxA);
-	colum("攻击力(小攻击) ATK: ",exp['atkSmall'],green,maxA);
-	colum("元素精通(增幅) 融化2.0 蒸发1.5 Elemental Mastery: ",exp['elemMastery'],green,maxA);
-	colum("暴击率 CRIT.Rate: ",exp['critRate'],green,maxA);
-	colum("暴击伤害 CRIT.DMG: ",exp['critDmg'],green,maxA);
-	// colum("元素精通(八重神子) 天赋 Elemental Mastery: ",exp['BaChong'],green,maxA);
-	ex.innerHTML+="<br>";
-
-	ex.innerHTML+=h("元素精通(单次剧变 90级 附着周期2.5s 高攻速加倍"+(RATIO*100)+"%技能倍率) Elemental Mastery: ");
-	colum("扩散(+60%伤害+40%减抗后) 两段/可能触发其他剧变",expEM['Overloaded']*0.3*2*2,orange,maxB);
-	colum("超载 火伤 爆炸",expEM['Overloaded'],orange,maxB);
-	colum("感电 共存 连续反应",expEM['Overloaded']*0.6,orange,maxB);
-	colum("超导 冰伤 降低物理抗性",expEM['Overloaded']*0.25,orange,maxB);
-	colum("冻结 碎冰",expEM['Overloaded']*0.75,orange,maxB);
-
-	// colum("八重神子(170.6%技能倍率)：超载 火伤 爆炸",expEM['BaChong'],orange,maxB);
-	// colum("八重神子(170.6%技能倍率)：感电 共存 连续反应",expEM['BaChong']*0.6,orange,maxB);
-	ex.innerHTML+="<br>";
-
-	hel1.innerHTML=h("健康范围(如果是NaN%，则不存在健康范围)：");
-	hel1.innerHTML+=p("暴击率 CRIT.Rate: ")
-		+div(healthy['critRate1'].toFixed(FixNum)+"%-"+healthy['critRate0'].toFixed(FixNum)+"%");
-	hel1.innerHTML+=p("暴击伤害 CRIT.DMG: ")
-		+div(healthy['critDmg1'].toFixed(FixNum)+"%-"+healthy['critDmg0'].toFixed(FixNum)+"%");
-
-	
-}
-function colum(text,num,color,max){
-	ex.innerHTML+=p(text)+colorDiv(num,color,max);
-}
-function bar(text,num){
-	ex.innerHTML+=p(text)+div(num);
-}
-function p(text){
-	return '<p>'
-		+text+'</p>';
-}
-function div(text,color='#f9f9f9'){
-	return '<div class="bg"><p class="num" style="'+
-	'background:'+color+';'
-	+'">'+text+'</p></div>';
-}
-function colorDiv(num,color='#f9f9f9',max){
-	return '<div class="bg"><p class="num" style="'+
-		'width:'+((100*num/max)>=100?100:(100*num/max))+'%;'+
-		'background:'+color+';'+
-		'">'+num.toFixed(FixNum)+'</p></div>';
-}
-function h(text){
-	return '<h1>'
-		+text+'</h1>';
-}
-
-
-console.log("\n\n\n\n\n        萌是深藏不漏的✿◡‿◡\n\n\n\n\n\ntip:修改ratio来修改技能倍率");
