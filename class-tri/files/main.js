4ae29f6d38f78f8b13165dece8815c64dbdee340
Cele <1575989756@qq.com>
2023-12-31 21:48:36 +0800

    update styles

--- class-tri/files/main.js
+++ class-tri/files/main.js
@@ -16,7 +16,26 @@ textColor = rootStyles.getPropertyValue('--textColor');
 borderColor = rootStyles.getPropertyValue('--borderColor');
 spotColor = rootStyles.getPropertyValue('--spotColor');
 tooltipColor = rootStyles.getPropertyValue('--tooltipColor');
-
+//获取位置
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+// } else {
+//     console.log('浏览器不支持Geolocation API');
+// }
+// function successCallback(position) {
+//     console.log('纬度：', position.coords.latitude);
+//     console.log('经度：', position.coords.longitude);
+//     console.log('高度：', position.coords.altitude);
+//     console.log('精度：', position.coords.accuracy);
+//     console.log('海拔高度：', position.coords.altitudeAccuracy);
+//     console.log('方向：', position.coords.heading);
+//     console.log('速度：', position.coords.speed);
+//     option.series[0].data.push({name: '我的位置', value: [position.coords.longitude, position.coords.latitude, '', 'A 暂无']});
+//     myChart.setOption(option, true);
+// }
+// function errorCallback(error) {
+//     console.log('获取地理位置信息失败：', error.message);
+// }
 // 判断当前操作系统是否为移动设备
 var os = function () {
     var ua = navigator.userAgent,
@@ -41,44 +60,59 @@ option = {
         text: '山东师大附中 2018 级 3 班毕业蹭饭地图',
         subtext: '',
         left:'center',
-        textStyle: { color: textColor, fontSize: 30 },
-        subtextStyle: { color: textColor, fontSize: 15 }
+        textStyle: {
+            color: textColor,
+            fontSize: 25
+        },
+        // subtextStyle: { color: textColor, fontSize: 15 }
     },
     tooltip: {
         trigger: 'item',
-        //triggerOn:'mousemove',
-        hideDelay: 333,
+        // triggerOn:'mousemove',
+        hideDelay: 300,
         confine: true, // 是否约束 content 在 viewRect 中。默认 false 是为了兼容以前版本。
+        borderColor : '#fff',
         textStyle: {
             fontWeight: 'regular',
             fontFamily: 'Yahei',
             fontSize: 22,
-            backgroundColor: 'rgba(40, 44, 52, 0.5)',
-            color: tooltipColor
+            // backgroundColor: 'rgba(40, 44, 52, 0.5)',
+            // color: tooltipColor
         },
-        formatter: function (params) { return params.name + '（' + params.value[3].split(' ')[1] + '）<br />' + params.value[2]; }
+        formatter: function (params) { 
+            return params.name 
+                + '（' + params.value[3].split(' ')[1] + '）<br/>' 
+                + params.value[2]; 
+        }
     },
     toolbox: {
-        show: true,
+        // show: false,
         orient: 'vertical',
-        itemSize: 32,
+        itemSize: 28,
         top: 'top',
-        left: 'right',
+        left: 'left',
         feature: {
             // saveAsImage: {},
-            // 这个自带的存图功能不能保存背景图，太丑，不要他了
-            // 但是图标挺好看的，可以直接修改对应的第 79191 行 echarts.js 代码
-            myAbout:
-            {
-                show: true,
-                title: '关于',
-                    icon: 'path://M25.5,42.7h-4.4c-1.3,0-3.3-0.5-5.3-1.5c-2-1-4.1-2.4-5.7-4.2l-4.4-4.8l-4.4-4.8c-0.4-0.4-0.8-1-0.9-1.7s0.1-1.6, 0.9-2.4c0.9-0.8, 1.8-1.1, 2.6-1.2s1.5, 0.1, 1.8, 0.2l2.9, 2.2l2.9, 2.2L11.7, 15l0.2-11.7c0- 0.4, 0.1-1.2, 0.5-1.8c0.5-0.6, 1.3-1.1, 2.9-1.1s2.5, 0.5, 3, 1.1C18.9, 2.1, 19, 2.8, 19, 3.3v8v8c0 - 1.1, 0.1-2.5, 0.5-3.6c0.4-1.2, 1.3-2, 2.8-2c1.5, 0, 2.5, 0.7, 3.1, 1.5c0.6, 0.8, 0.8, 1.8, 0.8, 2.5c0.1-0.3, 0.4-0.9, 1-1.4c0.6-0.5, 1.5-0.8, 2.7-0.8c1.6, 0, 2.5, 0.6, 3, 1.2c0.5, 0.6, 0.6, 1.4, 0.6, 1.7v0.5v0.5c0.2-0.3, 0.4-0.8, 0.9-1.2s1.3-0.7, 2.7-0.7c1.5, 0, 2.3, 0.4, 2.8, 0.9s0.6, 1.2, 0.6, 1.6v6.2v6.2c0, 1.8-1.1, 4.4-2.9, 6.6S33, 42.7, 30, 42.7l - 3.9 - 0.1',
+            // 这个自带的存图功能不能保存背景图
+            myAbout:{
+                title: '',
+                icon: 'M20,11H7.4l4.3-4.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L3.6,11.6c-0.4,0.4-0.4,1,0,1.4l6.7,6.7c0.4,0.4,1,0.4,1.4,0s0.4-1,0-1.4L7.4,13H20c0.6,0,1-0.4,1-1S20.6,11,20,11z',
+                // fill:'#000',
                 onclick: function () {
                     option.geo.roam = true;
-                    option.title.left = 'center';
+                    if(parentMaps.length > 0){
+                        changeMap(parentMaps[parentMaps.length - 1]);
+                        parentMaps.pop();
+                    }
                     option.title.text = '山东师大附中 2018 级 3 班毕业蹭饭地图';
                     myChart.setOption(option, true);
                 }
+            }
+        },
+        iconStyle: {
+            normal: {
+                borderColor: borderColor,
+                color: '#fff'
             }
         },
     },
@@ -86,56 +120,54 @@ option = {
         // zoom: 1.3,
         map: 'china',
         label: {
-            show: false
+            normal: {
+                show: false
+            },
+            emphasis:{
+                show: false
+            }
         },
-        // roam: true,
-        roam: os.isPc, // 如果 roam 开启 true，在手机上缩放会稍微有一点卡顿
+        //缩放
+        roam: true,
+        // roam: os.isPc, 
         itemStyle: {
             normal: {
                 areaColor: mainColor,
                 borderColor: borderColor
             },
             emphasis: {
-                areaColor: mainColor
+                areaColor: '#fff',
             }
         },
-        silent: false // 若 silent 设为 true，则鼠标移至相应地区则不会显示省份名，点也不会有反应
-
+        // silent: true // 若 silent 设为 true，则鼠标移至相应地区则不会显示省份名，点也不会有反应
     },
     series: [{
         name: '大学',
         type: 'scatter',
         coordinateSystem: 'geo',
         data: convertData(),
-        symbol: os.isPc ? 'pin' : 'circle',
-        // symbol: 'pin',
+        symbol: 'pin',
         symbolSize: 30,
         label: {
             normal: {
                 formatter: '{b}',
-                position: 'right',
-                show: false
+                // position: 'right',
+                show: true
             }
         },
         itemStyle: {
-            normal: {
-                color: spotColor
-            }
+            normal: {color: spotColor}
         }
-
-    },
-
-    ]
+    },]
 };
 
 
-var dontBack = false; // 用于实现双击返回上一级地图
 var parentMaps = new Array(); // 维护一个 array，用于记录地图路径
 
 function checkUrl() {
     var url = window.location.href;
     if (url.indexOf('#') == -1) return 'china';
-    place = url.substr(url.indexOf('#') + 1);
+    // place = url.substr(url.indexOf('#') + 1);
     place = decodeURI(place);
     if (MAPS.indexOf(place) != -1) {
         // dontBack = true;
@@ -145,7 +177,6 @@ function checkUrl() {
     }
     else return 'china';
 };
-
 option.geo.map = checkUrl();
 
 
@@ -161,42 +192,16 @@ if (option && typeof option === "object") {
 
 // 改变地图，传入新的地点
 function changeMap(newPlace) {
-    function changeSubtitle() {
-        var stuList = new Array();
-        for (var i = 0; i < DATA.length; ++i) {
-            if (DATA[i].city.indexOf(newPlace) != -1) {
-                var tmp = DATA[i].value.split(' ');
-                for (var j = 0; j < tmp.length; ++j) stuList.push(tmp[j]);
-            }
-        }
-        var str = '';
-        for (var i = 0; i < stuList.length; ++i) {
-            str += i == stuList.length - 1 ? stuList[i] : stuList[i] + ' ';
-            if ((i + 1) % 5 == 0) str += '\n\n';
-        }
-        // option.title.subtext = str;
-    }
-
-    option.title.left = 'center';
-    option.geo.label.show = (newPlace != 'china');
-    if(newPlace != 'china') {
+    if(newPlace != 'china')
         option.title.text = '山东师大附中 2018 级 3 班毕业蹭饭地图 - ' + newPlace;
-        changeSubtitle(newPlace);
-    }
-    else{
+    else
         option.title.text = '山东师大附中 2018 级 3 班毕业蹭饭地图';
-    }
     option.geo.map = newPlace;
     myChart.setOption(option, true);
 }
-
 myChart.on('click', function (params) {
-    if (params.name == '南海诸岛') {
-    }
+    // if (params.name == '南海诸岛') {}
     // dontBack = true;
-    // 显示对应省份的大图，并忽视对于校名大头钉的 click 事件
-    // 进入各省地图之后，可以显示城市的名字。
-    // 出来之后再关掉。
     if (MAPS.indexOf(params.name) != -1) {
         parentMaps.push(option.geo.map);
         changeMap(params.name);
@@ -204,25 +209,18 @@ myChart.on('click', function (params) {
 });
 
 // 由于拖拽地图也会触发 click 事件，所以这里判断一下鼠标按下和抬起的位置坐标变化
-var downX, downY;
-$('#main').mousedown(function (e) {
-    downX = e.pageX;
-    downY = e.pageY;
-});
-
-$('#main').mouseup(function (e) {
-    var upX = e.pageX;
-    var upY = e.pageY;
-    var dltX = Math.abs(upX - downX);
-    var dltY = Math.abs(upY - downY);
-    // console.log(dltX, dltY);
-    if (dltX <= 5 && dltY <= 5 && parentMaps.length > 0) {
-        if (!dontBack) {
-            changeMap(parentMaps[parentMaps.length - 1]);
-            parentMaps.pop();
-            // dontBack = true; // 保证双击返回上一层地图的重要操作
-        } else {
-            // dontBack = false;
-        }
-    }
-});
+// var downX, downY;
+// $('#main').mousedown(function (e) {
+//     downX = e.pageX;
+//     downY = e.pageY;
+// });
+// $('#main').mouseup(function (e) {
+//     var upX = e.pageX;
+//     var upY = e.pageY;
+//     var dltX = Math.abs(upX - downX);
+//     var dltY = Math.abs(upY - downY);
+//     if (dltX <= 5 && dltY <= 5 && parentMaps.length > 0) {
+//         changeMap(parentMaps[parentMaps.length - 1]);
+//         parentMaps.pop();
+//     }
+// });
