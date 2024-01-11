90dd67e3eede57b4f0206602d0bf5e6961420d59
Cele <1575989756@qq.com>
2024-01-11 23:17:59 +0800

    update

--- class-tri/files/main.js
+++ class-tri/files/main.js
@@ -1,5 +1,8 @@
-﻿var dom = document.getElementById("main");
-var myChart = echarts.init(dom);
+﻿var chartDom = document.getElementById("main");
+var myChart = echarts.init(chartDom, null, 
+    // 通过svg渲染，可以一试，反正兼容不了一点
+    // {renderer: 'svg'}
+);
 var rootStyles = getComputedStyle(document.documentElement);
 option = null;
 
@@ -9,31 +12,28 @@ var borderColor = '#029fd4';
 var spotColor = '#ff9070';
 var tooltipColor = '#c8c841';
 
+// 把css变量抓过来，方便修改
 mainColor = rootStyles.getPropertyValue('--mainColor');
 textColor = rootStyles.getPropertyValue('--textColor');
 borderColor = rootStyles.getPropertyValue('--borderColor');
 spotColor = rootStyles.getPropertyValue('--spotColor');
 tooltipColor = rootStyles.getPropertyValue('--tooltipColor');
-//获取位置
-// if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-// } else {
-//     console.log('浏览器不支持Geolocation API');
-// }
-// function successCallback(position) {
-//     console.log('纬度：', position.coords.latitude);
-//     console.log('经度：', position.coords.longitude);
-//     console.log('高度：', position.coords.altitude);
-//     console.log('精度：', position.coords.accuracy);
-//     console.log('海拔高度：', position.coords.altitudeAccuracy);
-//     console.log('方向：', position.coords.heading);
-//     console.log('速度：', position.coords.speed);
-//     option.series[0].data.push({name: '我的位置', value: [position.coords.longitude, position.coords.latitude, '', 'A 暂无']});
-//     myChart.setOption(option, true);
-// }
-// function errorCallback(error) {
-//     console.log('获取地理位置信息失败：', error.message);
-// }
+
+// 获取位置
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+}
+function successCallback(position) {
+    // console.log('纬度：', position.coords.latitude);
+    // console.log('经度：', position.coords.longitude);
+    // console.log('精度：', position.coords.accuracy);
+    option.series[0].data.push({name: '我的位置', value: [position.coords.longitude, position.coords.latitude, '', '某某省 您的定位所在位置']});
+    myChart.setOption(option);
+}
+function errorCallback(error) {
+    console.log('获取地理位置信息失败：', error.message);
+}
+
 // 判断当前操作系统是否为移动设备
 var os = function () {
     var ua = navigator.userAgent,
@@ -98,7 +98,7 @@ option = {
         }
     },
     toolbox: {
-        // show: false,
+        show: false,
         orient: 'vertical',
         itemSize: 28,
         top: 15,
@@ -107,9 +107,9 @@ option = {
             // saveAsImage: {},
             // 这个自带的存图功能不能保存背景图
             myAbout:{
-                title: '',
+                // title: '返回',
+                // icon是gpt生成的
                 icon: 'M20,11H7.4l4.3-4.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L3.6,11.6c-0.4,0.4-0.4,1,0,1.4l6.7,6.7c0.4,0.4,1,0.4,1.4,0s0.4-1,0-1.4L7.4,13H20c0.6,0,1-0.4,1-1S20.6,11,20,11z',
-                // fill:'#000',
                 onclick: function () {
                     option.geo.roam = true;
                     if(parentMaps.length > 0){
@@ -118,7 +118,7 @@ option = {
 						option.graphic[0].style.text = 
 							'{a|毕业蹭饭地图}\n\n{b|山东师大附中 2018 级 3 班'+(popPlace=='china'?'':' - '+popPlace)+'}';
                     }
-                    myChart.setOption(option, true);
+                    myChart.setOption(option);
                 }
             }
         },
@@ -157,7 +157,7 @@ option = {
                 areaColor: '#fff',
             }
         },
-        // silent: true // 若 silent 设为 true，则鼠标移至相应地区则不会显示省份名，点也不会有反应
+        // silent: true // 静默设为 true，则鼠标移至相应地区则不会显示省份名，点也不会有反应
     },
     series: [{
         name: '大学',
@@ -181,7 +181,7 @@ option = {
 
 
 var parentMaps = new Array(); // 维护一个 array，用于记录地图路径
-//e.g. http://127.0.0.1:3000/class-tri#威海
+//e.g. http://127.0.0.1/class-tri#威海
 function checkUrl() {
     var url = window.location.href;
     if (url.indexOf('#') == -1) return 'china';
@@ -198,7 +198,7 @@ function checkUrl() {
 option.geo.map = checkUrl();
 
 if (option && typeof option === "object") {
-    myChart.setOption(option, true);
+    myChart.setOption(option);
     myChart.resize();
     // setTimeout(function () {
     //     alert("使用说明：\n1) 点一下大学对应的橙色标记，会显示出在这个大学的同学名单\n2) 点一下每个省份的地图，将会进入到各个省份的高清大图模式\n3) 对于人数较多的区，也提供高清大图，可以点\n4) 点两下背景图片将返回到上一级地图\n5) 如果使用电脑查看此蹭饭图，还可以直接用鼠标滚轮进行缩放\n6) 地图右侧小手按钮重置地图\n7) 网页改编自 https://ssfz.top 山东师大附中 2018 级 10 班毕业蹭饭地图") 
@@ -212,15 +212,17 @@ function changeMap(newPlace) {
 			'{a|毕业蹭饭地图}\n\n{b|山东师大附中 2018 级 3 班 - '+newPlace+'}';
         option.geo.zoom = 1;
         option.geo.left = 'center';
+        option.toolbox.show = true;
     }
     else{
         option.graphic[0].style.text =
 			'{a|毕业蹭饭地图}\n\n{b|山东师大附中 2018 级 3 班}';
         option.geo.zoom = 2.5;
         option.geo.left = -100;
+        option.toolbox.show = false;
     }
-		
     option.geo.map = newPlace;
+    //去除了缩放动画
     myChart.setOption(option, true);
 }
 myChart.on('click', function (params) {
@@ -251,7 +253,7 @@ myChart.on('click', function (params) {
 // });
 // 
 
-// 监听zoom，用于设置scaleLimit
+// 监听zoom，近用于辅助设置scaleLimit，不要忘记注释掉
 // var zoomK=3;
 // myChart.on('georoam',function(params){
 //     var option = myChart.getOption();//获得option对象
