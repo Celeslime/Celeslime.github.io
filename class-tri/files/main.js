0ada8715ed7fb2ff7ca78ab83fbe0c6a166f514b
Cele <1575989756@qq.com>
2024-01-12 17:59:43 +0800

    update

--- class-tri/files/main.js
+++ class-tri/files/main.js
@@ -10,14 +10,13 @@ var mainColor = '#f3f8ff';
 var textColor = '#9da5b3';
 var borderColor = '#029fd4';
 var spotColor = '#ff9070';
-var tooltipColor = '#c8c841';
+var activeColor = '#fff';
 
 // 把css变量抓过来，方便修改
 mainColor = rootStyles.getPropertyValue('--mainColor');
 textColor = rootStyles.getPropertyValue('--textColor');
 borderColor = rootStyles.getPropertyValue('--borderColor');
 spotColor = rootStyles.getPropertyValue('--spotColor');
-tooltipColor = rootStyles.getPropertyValue('--tooltipColor');
 
 // 判断当前操作系统是否为移动设备
 var os = function () {
@@ -47,11 +46,12 @@ option = {
             text: '{a|毕业蹭饭地图}\n\n{b|山东师大附中 2018 级 3 班}',
             rich: {
                 a: {
-                    fontSize: '25px',
+                    fontSize: '22px',
+                    // fontFamily: '宋体',
                     fill: textColor, // 字体颜色
                     stroke: borderColor, // 描边颜色
                     lineWidth: 2, // 描边宽度
-					textAlign: 'center',
+					textAlign: 'center',// 内容居中
                 },
                 b: {
                     fontSize: '15px',
@@ -73,17 +73,15 @@ option = {
             fontWeight: 'regular',
             fontFamily: 'Yahei',
             fontSize: 22,
-            // backgroundColor: 'rgba(40, 44, 52, 0.5)',
-            // color: tooltipColor
         },
         formatter: function (params) { 
             return params.name 
-                + '（' + params.value[3].split(' ')[1] + '）<br/>' 
+                + ' - ' + params.value[3].split(' ')[1] + '<br/>' 
                 + params.value[2]; 
         }
     },
     toolbox: {
-        show: false,
+        // show: false,
         orient: 'vertical',
         itemSize: 28,
         top: 15,
@@ -102,8 +100,17 @@ option = {
                         var popPlace=parentMaps.pop();
 						option.graphic[0].style.text = 
 							'{a|毕业蹭饭地图}\n\n{b|山东师大附中 2018 级 3 班'+(popPlace=='china'?'':' - '+popPlace)+'}';
+                    } else {
+                        myChart.setOption(option,true);
+                        alert(
+                            "关于：\n"+
+                            "1. 点击学校橙色标记，列出该学校的同学名单\n"+
+                            "2. 点击省/市地图，进入到相应省/市的大图\n"+
+                            "3. 点击返回按钮回到上一级/重置地图/关于\n"+
+                            "4. 图表使用 echarts 制作，地图源于网络\n\n"+
+                            "联系方式：鸿 微信：wx1575989756"
+                        ); 
                     }
-                    myChart.setOption(option);
                 }
             }
         },
@@ -139,7 +146,7 @@ option = {
                 borderColor: borderColor
             },
             emphasis: {
-                areaColor: '#fff',
+                areaColor: activeColor,
             }
         },
         // silent: true // 静默设为 true，则鼠标移至相应地区则不会显示省份名，点也不会有反应
@@ -175,7 +182,6 @@ function checkUrl() {
     console.log(place);
     if (MAPS.indexOf(place) != -1) {
         parentMaps.push('china');
-        // option.geo.label.show = true;
         return place;
     }
     else return 'china';
@@ -185,9 +191,6 @@ option.geo.map = checkUrl();
 if (option && typeof option === "object") {
     myChart.setOption(option);
     myChart.resize();
-    // setTimeout(function () {
-    //     alert("使用说明：\n1) 点一下大学对应的橙色标记，会显示出在这个大学的同学名单\n2) 点一下每个省份的地图，将会进入到各个省份的高清大图模式\n3) 对于人数较多的区，也提供高清大图，可以点\n4) 点两下背景图片将返回到上一级地图\n5) 如果使用电脑查看此蹭饭图，还可以直接用鼠标滚轮进行缩放\n6) 地图右侧小手按钮重置地图\n7) 网页改编自 https://ssfz.top 山东师大附中 2018 级 10 班毕业蹭饭地图") 
-    // },500);
 }
 
 // 改变地图，传入新的地点
@@ -197,18 +200,17 @@ function changeMap(newPlace) {
 			'{a|毕业蹭饭地图}\n\n{b|山东师大附中 2018 级 3 班 - '+newPlace+'}';
         option.geo.zoom = 1;
         option.geo.left = 'center';
-        option.toolbox.show = true;
+        // option.toolbox.show = true;
     }
     else{
         option.graphic[0].style.text =
 			'{a|毕业蹭饭地图}\n\n{b|山东师大附中 2018 级 3 班}';
         option.geo.zoom = 2.5;
         option.geo.left = -100;
-        option.toolbox.show = false;
+        // option.toolbox.show = false;
     }
     option.geo.map = newPlace;
-    //去除了缩放动画
-    myChart.setOption(option, true);
+    myChart.setOption(option, true);    //去除了缩放动画
 }
 myChart.on('click', function (params) {
     if (params.name == '南海诸岛') {
@@ -220,23 +222,18 @@ myChart.on('click', function (params) {
     }
 });
 
-// 获取位置
+// 获取我的位置
 if (navigator.geolocation && os.isPc) {
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    navigator.geolocation.getCurrentPosition(successCallback);
 }
 function successCallback(position) {
-    // console.log('纬度：', position.coords.latitude);
-    // console.log('经度：', position.coords.longitude);
-    // console.log('精度：', position.coords.accuracy);
     option.series[0].data.push({
         name: '我的位置', 
         value: [position.coords.longitude, position.coords.latitude,'','某某省 定位所在位置']
     });
     myChart.setOption(option);
 }
-function errorCallback(error) {
-    console.log('获取地理位置信息失败：', error.message);
-}
+
 // 由于拖拽地图也会触发 click 事件，所以这里判断一下鼠标按下和抬起的位置坐标变化
 // var downX, downY;
 // $('#main').mousedown(function (e) {
